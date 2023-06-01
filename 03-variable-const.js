6179fbd4afaff10980d8f53cd51581e826a78bb7
Marcelo Toledo <marcelo.silva@arcadia.com>
2023-06-01 20:58:15 -0300

    Variables const

---
+++ 03-variable-const.js
@@ -0,0 +1,7 @@
+var num = 2;
+var obj = {
+    name: 'Marcelo Toledo',
+    last_name: 'Toledo'
+};
+obj.name = 'Marcelo';
+console.log(obj);
---
+++ 03-variable-const.ts
@@ -0,0 +1,10 @@
+const num = 2
+
+const obj = {
+  name: 'Marcelo Toledo',
+  last_name: 'Toledo'
+}
+
+obj.name = 'Marcelo'
+
+console.log(obj)
