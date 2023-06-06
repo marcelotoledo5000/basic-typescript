64d794e9ec10b83df91503a18d8bcf0766ee4972
Marcelo Toledo <marcelo.silva@arcadia.com>
2023-06-05 21:03:45 -0300

    Modifiers in classes

---
+++ 11-modifiers-in-classes.ts
@@ -0,0 +1,3 @@
+// public is public for everyone
+// private is accessible only by the class itself, nor to those who inherit
+// protected is accessible to the class itself and inheritors, but not available to the instance
